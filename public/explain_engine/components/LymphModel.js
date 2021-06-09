41e5a5fd6f679ee88b098f128a7a93b80220d3e5
dobutamine <tim.antonius@outlook.com>
2021-06-09 14:40:29 +0200

    update

--- public/explain_engine/components/LymphModel.js
+++ public/explain_engine/components/LymphModel.js
@@ -14,52 +14,8 @@ class LymphModel {
   }
 
   modelCycle() {
-    // fixed flow naar lymphagion
-
-    // elke model step een absoluet volume toevoegen
-    const flowP = 10
-
-    let volumeLB = this._model.components.LB.vol
-
-    let dvol = volumeLB * flowP / 100
-
-    this._model.components.LYMPHANGION.volIn(dvol, null, false)
-
-    this._model.components.LB.volOut(dvol)
-
-    let t = this._model.modeling_stepsize
-
-    this.tijd += t
 
 
-
-
-
-
-
-
-
-
-
-
-
-    // stepsize
-    // const t = this._model.modeling_stepsize
-
-    
-    // let volumeLB = this._model.components.LB.vol     // in liters
-
-    // // calculate flow as 10% of lower body
-    // let flowInThisStepExp = this.flowPercentage / 100 * volumeLB
-
-    // // remove volume from lower body and add to lymfangiom
-    // // change the volumes
-    // this._model.components.LB.volOut(flowInThisStepExp)
-
-    // this._model.components.LYMPHANGION.volIn(flowInThisStepExp, null, false)
-
-
-    
 
   }
 }
