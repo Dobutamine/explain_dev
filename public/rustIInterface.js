49fe303ca6575a7618dfd00d847b80167e1c4cf0
dobutamine <tim.antonius@outlook.com>
2021-08-19 17:15:50 +0200

    update structure

--- public/rustIInterface.js
+++ public/rustIInterface.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
-import init, { greet } from './pkg/rust_explain_engine'
+
+import init, { start_model, stop_model, calculate_model, fastforward_model, load_modeldefinition} from './pkg/rust_explain_engine'
 
 export class RustInterface {
   constructor () {
+    init()
 
-    init().then(() => {
-      // greet('WebAssembly is the greatest!')
-    })
+    // init().then(() => {
+    //   console.log('WASM module is initialized!')
+    // })
   }
+
+  loadModeldefinition () {
+    load_modeldefinition()
+  }
+
+  startModel () {
+    start_model()
+  }
+
+  stopModel () {
+    stop_model()
+  }
+
+  fastForwardModel () {
+    fastforward_model()
+  }
+
+  calculateModel () {
+    calculate_model()
+  }
+
 }
--- public/src/lib.rs
+++ public/src/lib.rs
@@ -1,11 +1,33 @@
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+#[wasm_bindgen(start)]
+pub fn run() {
+    // run some code when the wasm module starts
+    console::log_1(&"Rust/WASM module initialized and started correctly".into());
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn load_modeldefinition() {
+    console::log_1(&"Rust model definition loaded".into());
+}
+
+#[wasm_bindgen]
+pub fn start_model() {
+    console::log_1(&"Rust model started".into());
+}
+
+#[wasm_bindgen]
+pub fn stop_model() {
+    console::log_1(&"Rust model stopped".into());
+}
+
+#[wasm_bindgen]
+pub fn calculate_model() {
+    console::log_1(&"Rust model calculated".into());
+}
+
+#[wasm_bindgen]
+pub fn fastforward_model() {
+    console::log_1(&"Rust model fast forwarded".into());
 }
