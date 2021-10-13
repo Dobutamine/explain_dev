786383a32f6b7f21403d1cc2b8f4769f2c01a0f6
dobutamine <tim.antonius@outlook.com>
2021-10-13 23:24:00 +0200

    again

--- public/rustIInterface.js
+++ public/rustIInterface.js
@@ -1,40 +1,40 @@
-/* eslint-disable */
+// /* eslint-disable */
 
-import init, { start_model, stop_model, calculate_model, fastforward_model, load_modeldefinition } from './pkg/rust_explain_engine'
+// import init, { start_model, stop_model, calculate_model, fastforward_model, load_modeldefinition } from './pkg/rust_explain_engine'
 
-export class RustInterface {
-  initialized = false
+// export class RustInterface {
+//   initialized = false
 
-  constructor () {
-    // locate the wasm packages
-    const url = (import.meta.url).replace('/public', '/pkg')
+//   constructor () {
+//     // locate the wasm packages
+//     const url = (import.meta.url).replace('/public', '/pkg')
 
-    // build the url string to find the wasm packages
-    const input = new URL('rust_explain_engine_bg.wasm', url);
-    init(input).then(() => {
-      this.initialized = true
-      console.log('JS-Rust interface intialized.')
-    })
-  }
+//     // build the url string to find the wasm packages
+//     const input = new URL('rust_explain_engine_bg.wasm', url);
+//     init(input).then(() => {
+//       this.initialized = true
+//       console.log('JS-Rust interface intialized.')
+//     })
+//   }
 
-  loadModeldefinition (json) {
-    load_modeldefinition(json)
-  }
+//   loadModeldefinition (json) {
+//     load_modeldefinition(json)
+//   }
 
-  startModel () {
-    start_model()
-  }
+//   startModel () {
+//     start_model()
+//   }
 
-  stopModel () {
-    stop_model()
-  }
+//   stopModel () {
+//     stop_model()
+//   }
 
-  fastForwardModel () {
-    fastforward_model()
-  }
+//   fastForwardModel () {
+//     fastforward_model()
+//   }
 
-  calculateModel () {
-    calculate_model()
-  }
+//   calculateModel () {
+//     calculate_model()
+//   }
 
-}
+// }
